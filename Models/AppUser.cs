0bb8bcecff6793c44d4f183520d498b643da39a8
computhingss <zeynep.fidanay@gmail.com>
2024-09-18 12:01:39 +0300

    Add project files.

---
+++ Models/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CustomIdentity.Models
+{
+    public class AppUser:IdentityUser
+    {
+        public string? Name { get; set; }
+        public string? Address { get; set; }
+    }
+}
